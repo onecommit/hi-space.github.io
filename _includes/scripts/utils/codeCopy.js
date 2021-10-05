54fb5479f7386618523e76dd5945f539730309bc
hi-space <hispace.j@gmail.com>
2021-10-05 15:11:10 +0900

    copy code button style

--- _includes/scripts/utils/codeCopy.js
+++ _includes/scripts/utils/codeCopy.js
@@ -10,17 +10,25 @@ highlightBlocks.forEach(function (codeBlock) {
   copyButton.className = 'copy-code-button';
   copyButton.type = 'button';
   copyButton.ariaLabel = 'Copy code to clipboard';
+  
+  // clipboard icon
+  var icon = document.createElement('i');
+  icon.className = 'copy-code-icon'
+  icon.classList.add('far')
+  icon.classList.add('fa-clipboard')
+  copyButton.appendChild(icon)
 
   codeBlock.prepend(copyButton);
-
   copyButton.addEventListener('click', function () {
     var code = codeBlock.querySelector('td.rouge-code').innerText.trim();
     window.navigator.clipboard.writeText(code);
-
+    
+    icon.style.display = 'none'
     copyButton.classList.add('copied');
 
     setTimeout(function () {
+      icon.style.display = ''
       copyButton.classList.remove('copied');
-    }, 2000);
+    }, 1500);
   });
 });
