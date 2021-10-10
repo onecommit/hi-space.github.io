dee243e12ac1d9486cd62d20053be64cdeb19578
onecommit <oneday.onecommit@gmail.com>
2021-10-10 20:35:57 +0900

    Add Sidebar drawer feature
    Add Sidebar drawer feature

---
+++ _includes/scripts/components/sidebar-drawer.js
@@ -0,0 +1,16 @@
+// Side bar toggle button
+(function(document) {
+  var toggle = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('#sidebar');
+  var checkbox = document.querySelector('#sidebar-checkbox');
+
+  document.addEventListener('click', function(e) {
+    var target = e.target;
+
+    if(!checkbox.checked ||
+       sidebar.contains(target) ||
+       (target === checkbox || target === toggle)) return;
+
+    checkbox.checked = false;
+  }, false);
+})(document);
